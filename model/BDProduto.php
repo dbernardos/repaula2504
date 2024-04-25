05c1f80b69cdfdbf152169f0b608401bb8eafacd
dbernardos <davi.bernardo@ifsc.edu.br>
2024-04-25 15:57:21 -0300

    estrutura mvc

---
+++ model/BDProduto.php
@@ -0,0 +1,44 @@
+<?php
+
+class BDProduto{
+
+    private $pdo;
+
+    public function __construct(PDO $pdo){
+        $this->pdo = $pdo;
+    }
+
+    public function salvar(Produto $produto){
+        $sql = "INSERT INTO produto (descricao, quantidade, valor)
+                VALUES (:descricao, :quantidade, :valor)";
+        
+        $query = $this->pdo->prepare($sql);
+        $query->execute([
+            'descricao' => $produto->getDescricao(),
+            'quantidade' => $produto->getQuantidade(),
+            'valor' => $produto->getValor(),
+        ]);
+    }
+
+    public function buscar_produto($id){
+        $sql = 'SELECT * FROM produto WHERE codigo = :id';
+        $query = $this->pdo->prepare($sql);
+        $query->execute([
+            'codigo' => $id,
+        ]);
+
+        $produto = $query->fetchObject('Produto');
+        return $produto;
+    }
+
+    public function buscar_produtos(){
+        $sql = 'SELECT * FROM produto';
+        $resultado = $this->pdo->query($sql, PDO::FETCH_CLASS, 'Produto');
+        $produtos = [];
+
+        foreach($resultado as $prod){
+            $produtos[] = $prod;
+        }
+        return $produtos;
+    }
+}
