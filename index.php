7763da3c857fa0c79109ab7cec20786ad6ebe36f
dbernardos <davi.bernardo@ifsc.edu.br>
2024-04-25 16:28:12 -0300

    criação das rotas

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require "config.php";
+require "model/Produto.php";
+require "model/BDProduto.php";
+
+$bdproduto = new BDProduto($pdo);
+
+//rota padrão
+$rota = "produto";
+
+if(array_key_exists("rota", $_GET)){
+    $rota = (string) $_GET['rota'];
+}
+
+if(is_file("controller/{$rota}.php")){
+    require "controller/{$rota}.php";
+}else{
+    echo "Rota não encontrada";
+}
+?>
