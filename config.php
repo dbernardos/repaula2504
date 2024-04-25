25447d13ce19b8a04e19301401f4a1d811e9e19f
dbernardos <davi.bernardo@ifsc.edu.br>
2024-04-25 16:42:09 -0300

    correção senha

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $SENHABD = "123";
 $DSN = "mysql:dbname=dbproduto;host=localhost";
 
 try{
-    $pdo = new PDO($DSN, $USERBD, $SENHADB);
+    $pdo = new PDO($DSN, $USERBD, $SENHABD);
 } catch(PDOException $e){
     echo "Falha na conexão com o banco de dados: " . $e->getMessage();
     die();
