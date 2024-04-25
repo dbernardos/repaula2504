7a2106d4ea6673a14350afc0ea94327b7d0657c4
dbernardos <davi.bernardo@ifsc.edu.br>
2024-04-25 16:21:14 -0300

    criação da interface de produto

---
+++ view/ProdutoView.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_REQUEST['produtos'])){
+    $produtos = $_REQUEST['produtos'];
+}else{
+    echo "O array de produtos não foi recuperado";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Detalhes do Produto</title>
+    </head>
+
+    <body>
+        <table>
+            <tr>
+                <th>Codigo</th>
+                <th>Descricao</th>
+                <th>Quantidade</th>
+                <th>Valor</th>
+            </tr>
+            <?php foreach($produtos as $produto): ?>
+                <tr><?php echo $produto->getCodigo(); ?></tr>
+                <tr><?php echo $produto->getDescricao(); ?></tr>
+                <tr><?php echo $produto->getQuantidade(); ?></tr>
+                <tr><?php echo $produto->getValor(); ?></tr>
+            <?php endforeach; ?>
+        </table>
+    </body>
+</html>
